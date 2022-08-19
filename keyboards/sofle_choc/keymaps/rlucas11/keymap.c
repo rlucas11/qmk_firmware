fb99c5dc46406f68fac69df38a7618bac7b0cebe
rich <rlucas11@yahoo.com>
2022-08-19 10:30:29 -0400

    Changed layer switch keys on sofle_choc

--- keyboards/sofle_choc/keymaps/rlucas11/keymap.c
+++ keyboards/sofle_choc/keymaps/rlucas11/keymap.c
@@ -77,11 +77,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT( \
-    TD(TD_GRV_ESC),   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,      KC_0,         KC_MINS,
+    KC_GRV,           KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,      KC_0,         KC_MINS,
     KC_TAB,           KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,     KC_U,     KC_I,    KC_O,      KC_P,         KC_BSPC,
     KC_LSFT,        HOME_A, HOME_S,  HOME_D,  HOME_F,    KC_G,                               KC_H,   HOME_J,   HOME_K,  HOME_L, HOME_SCLN, RSFT_T(KC_QUOT),
     KC_LCTL,          KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,           KC_MPLY,KC_N,     KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  RCTL_T(KC_ENT),
-                      KC_CAPSLOCK, KC_LALT, KC_HYPR,LT(_LOWER, KC_ENTER),KC_ENT, KC_SPC, LT(_RAISE, KC_SPC), MO(_FUNCTION), KC_RALT, KC_RGUI
+                      KC_CAPSLOCK, KC_LALT, KC_APP, KC_HYPR,LT(_LOWER, KC_ENTER),LT(_RAISE, KC_SPC), MO(_FUNCTION), KC_ESC, KC_RALT, KC_RGUI    
 ),
 /* LOWER
  * ,----------------------------------------.                    ,-----------------------------------------.
